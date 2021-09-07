05d4e220970750336a56c86f5f29869ee91c6180
bedroesb <bedro@psb.vib-ugent.be>
2021-09-07 17:55:20 +0200

    deep linking towards tabs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,6 +32,31 @@ $(window).scroll(function () {
   }
 });
 
+/**
+ * Enabling deep linking towards tabs
+ */
+
+$(document).ready(() => {
+  let url = location.href.replace(/\/$/, "");
+
+  if (location.hash) {
+    const hash = url.split("#");
+    $('button[data-bs-target="#' + hash[1] + '"]').tab("show");
+    url = location.href.replace(/\/#/, "#");
+    history.replaceState(null, null, url);
+    setTimeout(() => {
+      $(window).scrollTop(0);
+    }, 400);
+  }
+
+  $('button[data-bs-toggle="tab"]').on("click", function () {
+    let newUrl;
+    const hash = $(this).attr("data-bs-target");
+    newUrl = url.split("#")[0] + hash;
+    newUrl += "/";
+    history.replaceState(null, null, newUrl);
+  });
+});
 
 /**
  * Topnav over headings fix
