28e9d185c94ce3ff83f826ae4fe5636a98a254f3
Alasdair Gray <A.J.G.Gray@hw.ac.uk>
2022-02-15 14:10:15 +0000

    Add logging

--- python/simplifyJSON.py
+++ python/simplifyJSON.py
@@ -1,5 +1,18 @@
+# Import logging
+import logging
 # Import regex library
 import re
+
+## Logging configuration
+logging.basicConfig(
+    filename='simplifyJSON.log',
+    filemode='w',
+    encoding='utf-8',
+    format='%(asctime)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+# Define location of JSON Schema files
+SCHEMA_LOC = "../_data/schemas/"
 
 def replaceDotsInFilename(filename):
     # Replace `.` in filename with `-` except for final `.json`
@@ -7,4 +20,5 @@ def replaceDotsInFilename(filename):
     return str
 
 #### Main
-print("../_data/schemas/" + replaceDotsInFilename("ComputationalTool_v1-0-RELEASE.json"))
+filename = SCHEMA_LOC + replaceDotsInFilename("ComputationalTool_v1-0-RELEASE.json")
+logging.info('Filename: %s' % filename)
